4484b011a1eedbf7bc4368239cf30202b91c4ae7
Sven Bohm <bohms@msu.edu>
2017-03-09 14:16:38 -0500

    have two graphs

--- main.elm
+++ main.elm
@@ -1,26 +1,13 @@
 module Main exposing (..)
 
+import Graph exposing (..)
 import Html
+import Html exposing (..)
 import Html exposing (program, Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
 import List.Extra as LE
-
-
-type alias Point =
-    { x : Float
-    , y : Float
-    , active : Bool
-    , id : Int
-    }
-
-
-type alias Incubation =
-    { co2 : List Point
-    , ch4 : List Point
-    , n2o : List Point
-    }
 
 
 type alias Model =
@@ -30,15 +17,11 @@ type alias Model =
     }
 
 
-type alias Axis =
-    { max_extent : Float
-    , max_value : Float
-    , min_value : Float
-    }
-
-
 type Msg
     = SwitchPoint Point
+    | SwitchCO2 Point
+    | SwitchCH4 Point
+    | NextIncubation
     | None
 
 
@@ -66,31 +49,34 @@ initialModel =
     }
 
 
-max_y : List Point -> Float
-max_y points =
-    case LE.maximumBy .y points of
-        Just point ->
-            point.y
+update_point : Point -> List Point -> List Point
+update_point point incubation =
+    let
+        old_list =
+            List.filter (\x -> x.id /= point.id) incubation
 
-        Nothing ->
-            120
-
-
-max_x : List Point -> Float
-max_x points =
-    case LE.maximumBy .x points of
-        Just point ->
-            point.x
-
-        Nothing ->
-            120
+        new_point =
+            { point | active = not point.active }
+    in
+        old_list ++ [ new_point ]
 
 
-axisTransform : Axis -> Float -> Float
-axisTransform axis value =
-    -- ((axis.max_value - axis.min_value)/(axis.max_extent - axis.min_extent) / value) * axis.max_extent
-    -- (axis.max_extent/(axis.max_value - axis.min_value) / value ) * axis.max_extent
-    (axis.max_extent / (axis.max_value - axis.min_value) * value)
+update_incubation_co2 : Incubation -> Point -> Incubation
+update_incubation_co2 incubation point =
+    let
+        new_points =
+            update_point point incubation.co2
+    in
+        { incubation | co2 = new_points }
+
+
+update_incubation_ch4 : Incubation -> Point -> Incubation
+update_incubation_ch4 incubation point =
+    let
+        new_points =
+            update_point point incubation.ch4
+    in
+        { incubation | ch4 = new_points }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -99,13 +85,27 @@ update msg model =
         None ->
             (model ! [])
 
+        SwitchCO2 point ->
+            let
+                new_incubation =
+                    update_incubation_co2 model.incubation point
+            in
+                ({ model | incubation = new_incubation } ! [])
+
+        SwitchCH4 point ->
+            let
+                new_incubation =
+                    update_incubation_ch4 model.incubation point
+            in
+                ({ model | incubation = new_incubation } ! [])
+
         SwitchPoint point ->
             let
                 incubation =
                     model.incubation
 
                 old_list =
-                    List.filter (\x -> x.id /= point.id) incubation.co2
+                    List.filter (\x -> x.id /= point.id) incubation.ch4
 
                 new_point =
                     { point | active = not point.active }
@@ -118,36 +118,26 @@ update msg model =
             in
                 ({ model | incubation = new_incubation } ! [])
 
-        UpdateData ->
+        NextIncubation ->
             (model ! [])
 
 
-viewBox_ : Model -> String
-viewBox_ model =
-    String.concat [ "0 0 ", (toString model.x_axis.max_extent), " ", (toString model.y_axis.max_extent) ]
+viewBox_ : Axis -> Axis -> String
+viewBox_ x_axis y_axis =
+    String.concat [ "0 0 ", (toString x_axis.max_extent), " ", (toString y_axis.max_extent) ]
 
 
-translateCoords : Model -> String
-translateCoords model =
-    String.concat [ "translate(0,", toString model.y_axis.max_extent, ") scale(1,-1)" ]
-
-
-dots : Model -> Point -> Svg Msg
-dots model point =
+dots : Axis -> Axis -> Msg -> Point -> Svg Msg
+dots x_axis y_axis msg point =
     let
         color =
-            case point.active of
-                True ->
-                    "black"
-
-                False ->
-                    "grey"
+            pointColor point.active
 
         x_axis_transform =
-            axisTransform model.x_axis
+            axisTransform x_axis
 
         y_axis_transform =
-            axisTransform model.y_axis
+            axisTransform y_axis
     in
         circle
             [ cx (toString (x_axis_transform point.x))
@@ -155,22 +145,49 @@ dots model point =
             , r "5"
             , stroke color
             , fill color
-            , onClick (SwitchPoint point)
+            , onClick msg
             ]
             []
 
 
-view : Model -> Html Msg
-view model =
+draw_graph_co2 : Axis -> Axis -> List Point -> Svg Msg
+draw_graph_co2 x_axis y_axis points =
     let
         dots_transform =
-            dots model
+            dots x_axis y_axis
     in
         svg
-            [ width (toString model.x_axis.max_extent), height (toString model.y_axis.max_extent), viewBox (viewBox_ model) ]
-            [ g [ transform (translateCoords model) ]
-                (List.map dots_transform model.incubation.co2)
+            [ width (toString x_axis.max_extent)
+            , height (toString y_axis.max_extent)
+            , viewBox (viewBox_ x_axis y_axis)
             ]
+            [ g [ transform (translateCoords y_axis) ]
+                (List.map (\x -> dots_transform (SwitchCO2 x) x) points)
+            ]
+
+
+draw_graph_ch4 : Axis -> Axis -> List Point -> Svg Msg
+draw_graph_ch4 x_axis y_axis points =
+    let
+        dots_transform =
+            dots x_axis y_axis
+    in
+        svg
+            [ width (toString x_axis.max_extent)
+            , height (toString y_axis.max_extent)
+            , viewBox (viewBox_ x_axis y_axis)
+            ]
+            [ g [ transform (translateCoords y_axis) ]
+                (List.map (\x -> dots_transform (SwitchCH4 x) x) points)
+            ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ draw_graph_co2 model.x_axis model.y_axis model.incubation.co2
+        , draw_graph_ch4 model.x_axis model.y_axis model.incubation.ch4
+        ]
 
 
 subscriptions : Model -> Sub Msg
