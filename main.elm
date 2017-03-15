a735855180549b4ddf4c5eee168bcd662106bce0
Sven Bohm <bohms@msu.edu>
2017-03-15 08:55:05 -0400

    compute max y

--- main.elm
+++ main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
-import Graph exposing (..)
+--import Graph exposing (..)
+
 import Date exposing (..)
 import Date.Extra as DE exposing (..)
 import Time exposing (..)
@@ -13,6 +14,65 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
 import SampleIncubation exposing (json, nextJson)
+import List.Extra as LE
+
+
+type alias Point =
+    { x : Float
+    , y : Float
+    , deleted : Bool
+    , id : Int
+    }
+
+
+type alias Flux =
+    { points : List Point
+    , xAxis : Axis
+    , yAxis : Axis
+    , slope : Float
+    , intercept : Float
+    }
+
+
+type alias Incubation =
+    { co2 : Flux
+    , ch4 : Flux
+    , n2o : Flux
+    , injections : List Injection
+    }
+
+
+
+-- type alias Incubation =
+--     { co2 : List Point
+--     , ch4 : List Point
+--     , n2o : List Point
+--     , injections : List Injection
+--     }
+
+
+type alias Axis =
+    { max_extent : Float
+    , min_value : Float
+    , max_value : Float
+    }
+
+
+type alias Model =
+    { incubation : Incubation
+    , next_incubation : Incubation
+    , status : Status
+    }
+
+
+type alias Injection =
+    { co2_ppm : Float
+    , n2o_ppm : Float
+    , ch4_ppm : Float
+    , id : Int
+    , deleted : Bool
+    , datetime : Date
+    }
 
 
 type Status
@@ -20,17 +80,6 @@ type Status
     | Bad
     | MaybeGood
     | NotChecked
-
-
-type alias Model =
-    { incubation : Incubation
-    , next_incubation : Incubation
-    , x_axis : Axis
-    , y_axis_co2 : Axis
-    , y_axis_ch4 : Axis
-    , y_axis_n2o : Axis
-    , status : Status
-    }
 
 
 type Msg
@@ -47,21 +96,7 @@ initialModel : Model
 initialModel =
     { incubation = toIncubation (decodeInjections SampleIncubation.json)
     , next_incubation = toIncubation (decodeInjections SampleIncubation.nextJson)
-    , x_axis = Axis 220 100 0
-    , y_axis_co2 = Axis 120 200 0
-    , y_axis_ch4 = Axis 120 3 0
-    , y_axis_n2o = Axis 120 1 0
     , status = NotChecked
-    }
-
-
-type alias Injection =
-    { co2_ppm : Float
-    , n2o_ppm : Float
-    , ch4_ppm : Float
-    , id : Int
-    , deleted : Bool
-    , datetime : Date
     }
 
 
@@ -87,34 +122,82 @@ initialTime injections =
                 Date.fromTime (Time.inSeconds 0)
 
 
+interval : Date -> Date -> Float
+interval startTime time =
+    ((Date.toTime time) - Date.toTime (startTime)) / 1000 / 60
+
+
+updateInjection : List Injection -> Int -> Bool -> List Injection
+updateInjection injections id active =
+    let
+        ( incubation, rest ) =
+            List.partition (\x -> x.id == id) injections
+
+        newIncubation =
+            case List.head incubation of
+                Just incubation ->
+                    [ { incubation | deleted = active } ]
+
+                Nothing ->
+                    []
+    in
+        List.concat [ rest, newIncubation ]
+
+
+
+-- toInjection : List Incubation -> List Incubation
+-- toInjection incubation =
+--     List.map (\x -> updateInjection incubation.injections x.id x.active) incubation.co2
+-- toFlux : List Injection  -> Flux
+-- toFlux : injections =
+
+
 toIncubation : List Injection -> Incubation
 toIncubation injections =
     let
-        interval time =
-            ((Date.toTime time) - Date.toTime (initialTime injections)) / 1000 / 60
+        startTime =
+            initialTime injections
 
-        co2s =
+        pointInterval =
+            interval startTime
+
+        xAxis =
+            Axis 200 0 200
+
+        yAxis =
+            Axis 120 0
+
+        co2Points =
             List.map
                 (\x ->
-                    Point (interval x.datetime) x.co2_ppm x.deleted x.id
+                    Point (pointInterval x.datetime) x.co2_ppm x.deleted x.id
+                )
+                injections
+
+        co2s =
+            Flux co2Points xAxis (yAxis (maxY co2Points)) 0 0
+
+        ch4Points =
+            List.map
+                (\x ->
+                    Point (pointInterval x.datetime) x.ch4_ppm x.deleted x.id
                 )
                 injections
 
         ch4s =
+            Flux ch4Points xAxis (yAxis (maxY ch4Points)) 0 0
+
+        n2oPoints =
             List.map
                 (\x ->
-                    Point (interval x.datetime) x.ch4_ppm True x.id
+                    Point (pointInterval x.datetime) x.n2o_ppm x.deleted x.id
                 )
                 injections
 
         n2os =
-            List.map
-                (\x ->
-                    Point (interval x.datetime) x.n2o_ppm True x.id
-                )
-                injections
+            Flux n2oPoints xAxis (yAxis (maxY n2oPoints)) 0 0
     in
-        Incubation co2s ch4s n2os
+        Incubation co2s ch4s n2os injections
 
 
 
@@ -163,6 +246,55 @@ decodeInjections json =
             []
 
 
+
+--- VIEW
+
+
+maxY : List Point -> Float
+maxY points =
+    case LE.maximumBy .y points of
+        Just point ->
+            point.y
+
+        Nothing ->
+            120
+
+
+maxX : List Point -> Float
+maxX points =
+    case LE.maximumBy .x points of
+        Just point ->
+            point.x
+
+        Nothing ->
+            120
+
+
+axisTransform : Axis -> Float -> Float
+axisTransform axis value =
+    (axis.max_extent / (axis.max_value - axis.min_value) * value)
+
+
+pointColor : Bool -> String
+pointColor deleted =
+    case deleted of
+        True ->
+            "grey"
+
+        False ->
+            "blue"
+
+
+translateCoords : Axis -> String
+translateCoords axis =
+    String.concat [ "translate(0,", toString axis.max_extent, ") scale(1,-1)" ]
+
+
+viewBox_ : Axis -> Axis -> String
+viewBox_ x_axis y_axis =
+    String.concat [ "0 0 ", (toString x_axis.max_extent), " ", (toString y_axis.max_extent) ]
+
+
 update_point : Point -> List Point -> List Point
 update_point point incubation =
     let
@@ -170,7 +302,7 @@ update_point point incubation =
             List.filter (\x -> x.id /= point.id) incubation
 
         new_point =
-            { point | active = not point.active }
+            { point | deleted = not point.deleted }
     in
         old_list ++ [ new_point ]
 
@@ -178,28 +310,46 @@ update_point point incubation =
 update_incubation_co2 : Incubation -> Point -> Incubation
 update_incubation_co2 incubation point =
     let
-        new_points =
-            update_point point incubation.co2
+        co2 =
+            incubation.co2
+
+        newPoints =
+            update_point point co2.points
+
+        newCO2 =
+            { co2 | points = newPoints }
     in
-        { incubation | co2 = new_points }
+        { incubation | co2 = newCO2 }
 
 
 update_incubation_ch4 : Incubation -> Point -> Incubation
 update_incubation_ch4 incubation point =
     let
-        new_points =
-            update_point point incubation.ch4
+        ch4 =
+            incubation.ch4
+
+        newPoints =
+            update_point point ch4.points
+
+        newCH4 =
+            { ch4 | points = newPoints }
     in
-        { incubation | ch4 = new_points }
+        { incubation | ch4 = newCH4 }
 
 
 update_incubation_n2o : Incubation -> Point -> Incubation
 update_incubation_n2o incubation point =
     let
-        new_points =
-            update_point point incubation.n2o
+        n2o =
+            incubation.n2o
+
+        newPoints =
+            update_point point n2o.points
+
+        newN2O =
+            { n2o | points = newPoints }
     in
-        { incubation | n2o = new_points }
+        { incubation | n2o = newN2O }
 
 
 swapIncubation : Model -> Model
@@ -289,7 +439,7 @@ dots : Axis -> Axis -> Msg -> Point -> Svg Msg
 dots x_axis y_axis msg point =
     let
         color =
-            pointColor point.active
+            pointColor point.deleted
 
         x_axis_transform =
             axisTransform x_axis
@@ -308,8 +458,8 @@ dots x_axis y_axis msg point =
             []
 
 
-draw_graph : List (Svg Msg) -> Axis -> Axis -> List Point -> Svg Msg
-draw_graph drawing_func x_axis y_axis points =
+draw_graph : List (Svg Msg) -> Axis -> Axis -> Svg Msg
+draw_graph drawing_func x_axis y_axis =
     svg
         [ width (toString x_axis.max_extent)
         , height (toString y_axis.max_extent)
@@ -350,20 +500,29 @@ n2o_dots x_axis y_axis points =
 view : Model -> Html Msg
 view model =
     let
+        n2o =
+            model.incubation.n2o
+
+        co2 =
+            model.incubation.co2
+
+        ch4 =
+            model.incubation.ch4
+
         dots_n2o =
-            n2o_dots model.x_axis model.y_axis_n2o model.incubation.n2o
+            n2o_dots n2o.xAxis n2o.yAxis n2o.points
 
         dots_co2 =
-            co2_dots model.x_axis model.y_axis_co2 model.incubation.co2
+            co2_dots co2.xAxis co2.yAxis co2.points
 
         dots_ch4 =
-            ch4_dots model.x_axis model.y_axis_ch4 model.incubation.ch4
+            ch4_dots ch4.xAxis ch4.yAxis ch4.points
     in
         div []
             [ div []
-                [ draw_graph dots_co2 model.x_axis model.y_axis_co2 model.incubation.co2
-                , draw_graph dots_ch4 model.x_axis model.y_axis_ch4 model.incubation.ch4
-                , draw_graph dots_n2o model.x_axis model.y_axis_n2o model.incubation.n2o
+                [ draw_graph dots_co2 co2.xAxis co2.yAxis
+                , draw_graph dots_ch4 ch4.xAxis ch4.yAxis
+                , draw_graph dots_n2o n2o.xAxis n2o.yAxis
                 ]
             , button [ onClick (FluxGood model.incubation) ] [ Html.text "Good" ]
             , button [ onClick (FluxMaybeGood model.incubation) ] [ Html.text "Maybe" ]
